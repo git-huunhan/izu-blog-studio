ed1616ee20162098193fabd1aba4955f83b5a989
Huu Nhan <huunhankirigamer@gmail.com>
2021-08-24 20:54:49 +0700

    Add schemaType for BlogDetailPage, custom fields with luv :heart:

--- schemas/schema.js
+++ schemas/schema.js
@@ -12,6 +12,23 @@ export default createSchema({
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
     {
+      name: "author",
+      type: "document",
+      title: "Author",
+      fields: [
+        {
+          name: "name",
+          title: "Name",
+          type: "string",
+        },
+        {
+          name: "avatar",
+          title: "Avatar",
+          type: "image",
+        },
+      ],
+    },
+    {
       name: "blog",
       type: "document",
       title: "Blog",
@@ -27,9 +44,93 @@ export default createSchema({
           title: "Subtitle",
         },
         {
+          name: "coverImage",
+          title: "Cover Image",
+          type: "image",
+          fields: [
+            {
+              type: "text",
+              name: "alt",
+              title: "Description",
+            },
+          ],
+          options: {
+            hotspot: true,
+          },
+        },
+        {
+          name: "content",
+          title: "Content",
+          type: "array",
+          of: [
+            {
+              type: "block",
+            },
+            {
+              type: "image",
+              fields: [
+                {
+                  title: "Image Position",
+                  name: "position",
+                  type: "string",
+                  options: {
+                    list: [
+                      {
+                        title: "Left",
+                        value: "left",
+                      },
+                      {
+                        title: "Center",
+                        value: "center",
+                      },
+                      {
+                        title: "Right",
+                        value: "right",
+                      },
+                    ],
+                    layout: "radio",
+                    isHighlighted: true,
+                  },
+                },
+                {
+                  type: "text",
+                  name: "alt",
+                  title: "Description",
+                  options: {
+                    isHighlighted: true,
+                  },
+                },
+              ],
+              options: {
+                hotspot: true,
+              },
+            },
+            {
+              type: "code",
+              options: {
+                withFilename: true,
+              },
+            },
+          ],
+        },
+        {
+          name: "date",
+          title: "Date",
+          type: "datetime",
+          validation: (Rule) => Rule.required(),
+        },
+        {
+          name: "author",
+          title: "Author",
+          type: "reference",
+          to: [{ type: "author" }],
+          validation: (Rule) => Rule.required(),
+        },
+        {
           name: "slug",
           type: "slug",
           title: "Slug",
+          validation: (Rule) => Rule.required(),
         },
       ],
     },
